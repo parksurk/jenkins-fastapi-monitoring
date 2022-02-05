398f15c5a695247a29249a4b92f868687ac95c0d
parksurk <parksurk@gmail.com>
2022-02-05 20:13:15 +0900

    update api.py with predict and healthcheck method

--- app/api.py
+++ app/api.py
@@ -16,3 +16,17 @@ instrumentator.instrument(app).expose(app, include_in_schema=False, should_gzip=
 @app.get("/")
 def root():
     return "Wine Quality Ratings"
+
+@app.post("/predict", response_model=Rating)
+def predict(response: Response, sample: Wine):
+    sample_dict = sample.dict()
+    features = np.array([sample_dict[f] for f in feature_names]).reshape(1, -1)
+    features_scaled = scaler.transform(features)
+    prediction = model.predict(features_scaled)[0]
+    response.headers["X-model-score"] = str(prediction)
+    return Rating(quality=prediction)
+
+
+@app.get("/healthcheck")
+def healthcheck():
+    return {"status": "ok"}
